2fc5cc357342ae25c71c63d0188e181f19d240ad
Aadi-Indium <126691041+Aadi-Indium@users.noreply.github.com>
2023-03-01 20:33:32 +0530

    basics programs

---
+++ Number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Number
+{
+public static void main(String[] args)
+{
+Scanner a=new Scanner(System.in);
+System.out.println("enter the number");
+int num=a.nextInt();
+if num>0
+{
+
+System.out.println(num+"the value is positive");
+
+}
+else
+
+{
+System.out.println(num+"the value is negative");
+}
+
+}
---
+++ Perfect.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Perfect
+{
+public static void main(String[]args)
+{
+Scanner ab=new Scanner(System.in);
+System.out.println("Enter a number");
+int num=ab.nextInt();
+int sum=0;
+for(int i=1;i<=num;i++)
+{
+if(num%i==0)
+{
+sum=sum+i;
+}
+}
+if(sum==num)
+{
+System.out.println("the num is a perfect num");
+}
+else{
+System.out.println("the num is not a perfect num");
+}
+}
+}
